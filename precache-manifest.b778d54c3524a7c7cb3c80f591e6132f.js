3fd17f4de4a80d45e1b7d76009ed7f37d5ac4c70
Adam Horscraft <Adam@Adams-MacBook-Pro-2.local>
2020-07-28 14:04:51 +0100

    Updates

---
+++ precache-manifest.b778d54c3524a7c7cb3c80f591e6132f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff9552adce9cac87dcad92272f0ce480",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "ad5532b5a4d96679b674",
+    "url": "/todolist/static/css/main.ffc12842.chunk.css"
+  },
+  {
+    "revision": "f3b33a566712a8656ce0",
+    "url": "/todolist/static/js/2.b798610f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.b798610f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad5532b5a4d96679b674",
+    "url": "/todolist/static/js/main.a020ce70.chunk.js"
+  },
+  {
+    "revision": "197f51c8578d731c6831",
+    "url": "/todolist/static/js/runtime-main.e7725399.js"
+  }
+]);
